a015df9bb531d1667c341ebde43aa1007b7b1fef
Ishan-sa <ishansach19@gmail.com>
2023-03-03 00:48:04 -0800

    basic prisma setup done

---
+++ prisma/migrations/20230303084713_init/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT,
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+);
