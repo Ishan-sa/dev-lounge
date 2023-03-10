1cbea92543a92d671352f8d4ec9e27a69030117e
Ishan-sa <ishansach19@gmail.com>
2023-03-10 14:01:15 -0800

    ssr fixed

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,12 +3,11 @@ import { NextUIProvider, createTheme } from "@nextui-org/react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import Nav from "../components/Navbar/Navbar";
 import { SessionProvider } from "next-auth/react";
-import { useSession, signIn, signOut } from "next-auth/react";
 import { getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]";
 import { MDXProvider } from "@mdx-js/react";
-import Image from "next/image";
 import MDXComponents from "@/components/MDX-Styles/MDX-Styles";
+import { useSSR } from "@nextui-org/react";
 
 // 2. Call `createTheme` and pass your custom values
 const lightTheme = createTheme({
@@ -25,26 +24,30 @@ export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }) {
-  // const { data: session } = useSession();
+  const { isBrowser } = useSSR();
   return (
     <>
-      <SessionProvider session={session}>
-        <NextThemesProvider
-          defaultTheme="system"
-          attribute="class"
-          value={{
-            light: lightTheme.className,
-            dark: darkTheme.className,
-          }}
-        >
+      {isBrowser && (
+        <>
           <NextUIProvider>
-            <Nav />
-            <MDXProvider components={MDXComponents}>
-              <Component {...pageProps} />
-            </MDXProvider>
+            <SessionProvider session={session}>
+              <NextThemesProvider
+                defaultTheme="system"
+                attribute="class"
+                value={{
+                  light: lightTheme.className,
+                  dark: darkTheme.className,
+                }}
+              >
+                <Nav />
+                <MDXProvider components={MDXComponents}>
+                  <Component {...pageProps} />
+                </MDXProvider>
+              </NextThemesProvider>
+            </SessionProvider>
           </NextUIProvider>
-        </NextThemesProvider>
-      </SessionProvider>
+        </>
+      )}
     </>
   );
 }
