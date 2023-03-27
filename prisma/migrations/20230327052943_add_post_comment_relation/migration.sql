22ac59c0bb294c6d526979889ed6339a3d5a4ead
Ishan-sa <ishansach19@gmail.com>
2023-03-26 23:01:17 -0700

    some shit

--- prisma/migrations/20230327052943_add_post_comment_relation/migration.sql
+++ prisma/migrations/20230327052943_add_post_comment_relation/migration.sql
@@ -6,3 +6,6 @@
 */
 -- AlterTable
 ALTER TABLE "Comment" ADD COLUMN     "title" TEXT NOT NULL;
+
+-- AddForeignKey
+ALTER TABLE "Comment" ADD CONSTRAINT "Comment_postId_fkey" FOREIGN KEY ("postId") REFERENCES "Post"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
