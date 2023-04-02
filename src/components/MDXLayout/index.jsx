f61c244b4066401c2f422f95d30de2a9c6905b58
Ishan-sa <ishansach19@gmail.com>
2023-04-01 18:20:46 -0700

    💀

--- src/components/MDXLayout/index.jsx
+++ src/components/MDXLayout/index.jsx
@@ -1,7 +1,9 @@
 import useSWR, { mutate } from "swr";
 import { useRouter } from "next/router";
+import Card1 from "../Card/Card";
 
 import CommentsSection from "../CommentsSection/CommentsSection";
+import { useEffect } from "react";
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
@@ -12,6 +14,8 @@ export default function Layout({ children }) {
 
   const { data: post } = useSWR(`/api/post?slug=${slug}`, fetcher);
 
+  const { data: posts } = useSWR(`/api/post`, fetcher);
+
   return (
     <div className="flex flex-col px-8 max-w-[800px] m-auto">
       {children}
@@ -21,6 +25,40 @@ export default function Layout({ children }) {
           onMutate={() => mutate(`/api/post?slug=${slug}`)}
         />
       )}
+
+      <div className="flex">
+        <h1 className="text-2xl font-semibold">Explore other articles</h1>
+      </div>
+      <div className="flex flex-col gap-2 my-4">
+        <div className="flex flex-col gap-2 lg:flex-row">
+          {posts &&
+            posts
+              .slice(0, 2)
+              .map((post) => (
+                <Card1
+                  key={post.id}
+                  topHeader={post.topHeader}
+                  title={post.title}
+                  image={post.image}
+                  onClick={() => router.push(`/articles/${post.slug}`)}
+                />
+              ))}
+        </div>
+        <div className="flex flex-col gap-2 lg:flex-row">
+          {posts &&
+            posts
+              .slice(2, 4)
+              .map((post) => (
+                <Card1
+                  key={post.id}
+                  topHeader={post.topHeader}
+                  title={post.title}
+                  image={post.image}
+                  onClick={() => router.push(`/articles/${post.slug}`)}
+                />
+              ))}
+        </div>
+      </div>
     </div>
   );
 }
