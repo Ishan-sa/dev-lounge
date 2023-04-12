a0986ec71bbd99a445190dee9addefe9904be2bc
Ishan-sa <ishansach19@gmail.com>
2023-04-11 22:07:00 -0700

    copy btn added

---
+++ src/components/CodeBlock/CodeBlock.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Prism from "prismjs";
+import "prism-themes/themes/prism-dracula.css";
+import CopyButton from "../CopyButton/CopyButton";
+import "prismjs/components/prism-jsx";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+function CodeBlock({ children, className }) {
+  const codeRef = React.useRef(null);
+  const language = className ? className.replace(/language-/, "") : "";
+  const text = children.trim();
+
+  React.useEffect(() => {
+    Prism.highlightElement(codeRef.current);
+  }, []);
+
+  const handleCopy = async () => {
+    try {
+      await navigator.clipboard.writeText(text);
+      notifyCopy();
+    } catch (err) {
+      console.error("Failed to copy text: ", err);
+    }
+  };
+
+  const notifyCopy = () => {
+    toast("Copied to clipboard", {
+      position: "top-right",
+    });
+  };
+
+  return (
+    <div className="relative">
+      <CopyButton
+        text={text}
+        className="absolute top-0 right-0 m-[1rem] text-[#3e5c9d] text-2xl hover:text-[#6797ff] duration-200 cursor-pointer"
+        onClick={handleCopy}
+      />
+      <pre className={`language-${language}`}>
+        <code ref={codeRef} className={`language-${language}`}>
+          {text}
+        </code>
+      </pre>
+    </div>
+  );
+}
+
+CodeBlock.propTypes = {
+  children: PropTypes.node.isRequired,
+  className: PropTypes.string,
+};
+
+export default CodeBlock;
---
+++ src/components/CopyButton/CopyButton.jsx
@@ -0,0 +1,16 @@
+import { IoCopyOutline } from "react-icons/io5";
+
+function CopyButton({ text, className = "", onClick = () => {} }) {
+  const handleCopy = async () => {
+    try {
+      await navigator.clipboard.writeText(text);
+      console.log("Text copied to clipboard");
+    } catch (err) {
+      console.error("Failed to copy text: ", err);
+    }
+  };
+
+  return <IoCopyOutline className={className} onClick={onClick} />;
+}
+
+export default CopyButton;
