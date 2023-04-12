cb247ccb96c82a6e7c1fe677639c1794b69f6d04
Ishan-sa <ishansach19@gmail.com>
2023-04-11 23:37:17 -0700

    copy btn added

--- src/components/CopyButton/CopyButton.jsx
+++ src/components/CopyButton/CopyButton.jsx
@@ -1,12 +1,9 @@
 import { IoCopyOutline } from "react-icons/io5";
 
-function CopyButton({ onClick = () => {} }) {
+function CopyButton({ onClick = () => {}, content }) {
   return (
     <>
-      <IoCopyOutline
-        className="absolute top-0 right-0 m-[1rem] text-[#7f8ea2] text-3xl hover:text-[#9aacc4] duration-200 cursor-pointer bg-[#3f4750] p-[5px] rounded-lg hover:bg-[#58626e]"
-        onClick={onClick}
-      />
+      <div onClick={onClick}>{content}</div>
     </>
   );
 }
